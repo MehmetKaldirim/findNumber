a84110abf81319e79a5668181c1426275bc5ba10
Mehmet <mathdenizi@gmail.com>
2024-05-19 17:08:43 +0200

    adding gradient and background image

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     marginTop: 100,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elavation: 4,
     shadowColor: "black",
