2a0f2dc39b62391c85ce9325a04d296082983371
Mehmet <mathdenizi@gmail.com>
2024-05-21 09:35:54 +0200

    usage of keyboard avoiding view and scrollview

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,32 +1,47 @@
-import { Text, View, StyleSheet, Image, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
   return (
-    <View style={styles.rootContainer}>
-      <Title>Game is over!</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require("../assets/images/success.png")}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>Game is over!</Title>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require("../assets/images/success.png")}
+          />
+        </View>
+        <View>
+          <Text style={styles.summaryText}>
+            Your phone needed{" "}
+            <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess
+            the number <Text style={styles.highlight}>{userNumber}</Text>.
+          </Text>
+          <PrimaryButton onPress={onStartNewGame}>
+            Start a new Game
+          </PrimaryButton>
+        </View>
       </View>
-      <View>
-        <Text style={styles.summaryText}>
-          Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
-          rounds to guess the number{" "}
-          <Text style={styles.highlight}>{userNumber}</Text>.
-        </Text>
-        <PrimaryButton onPress={onStartNewGame}>Start a new Game</PrimaryButton>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
 const deviceWidth = Dimensions.get("window").width;
+console.log(deviceWidth);
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -34,8 +49,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 200 : deviceWidth > 680 ? 450 : 300,
-    height: deviceWidth < 380 ? 200 : deviceWidth > 680 ? 450 : 300,
+    width: deviceWidth < 380 ? 200 : deviceWidth > 852 ? 450 : 300,
+    height: deviceWidth < 380 ? 200 : deviceWidth > 852 ? 450 : 300,
     borderRadius: deviceWidth < 380 ? 90 : 180,
     borderWidth: 3,
     borderColor: Colors.primary800,
